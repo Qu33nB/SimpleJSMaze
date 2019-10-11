a34475e22d9a5af6d1bd5b659d464a68075f8a95
Brandi Cotton <bdcotton52@gmail.com>
2019-10-10 22:25:51 -0500

    Fixed movement

--- maze.js
+++ maze.js
@@ -22,7 +22,7 @@ let row, rows, cell, cells, player;
 let playerClass = document.getElementsByClassName('player');
 let playerTop = 0;
 let playerLeft = 0;
-let playerRow = 10;
+let playerRow = 9;
 let playerColumn = 0;
 
 document.addEventListener('keydown', movePlayer);
@@ -72,23 +72,23 @@ addPlayer();
 function movePlayer(event) {
 
     if (event.keyCode === 40) {
-        if (mapArray[playerRow + 1][playerColumn] === 'W') {
+        if (mapArray[playerRow + 1][playerColumn] !== 'W') {
             player.style.top = (playerTop += 35) + "px";
             playerRow += 1;
         }
     } else if (event.keyCode === 38) {
-        if (mapArray[playerRow - 1][playerColumn] === 'W') {
+        if (mapArray[playerRow - 1][playerColumn] !== 'W') {
             player.style.top = (playerTop -= 35) + "px";
             playerRow -= 1;
         }
     } else if (event.keyCode === 39) {
-        if (mapArray[playerRow][playerColumn + 1] === 'W') {
-            player.style.left = (playerLeft += 34) + "px";
+        if (mapArray[playerRow][playerColumn + 1] !== 'W') {
+            player.style.left = (playerLeft += 30) + "px";
             playerColumn += 1;
         }
     } else if (event.keyCode === 37) {
-        if (mapArray[playerRow][playerColumn - 1] === 'W') {
-            player.style.left = (playerLeft -= 34) + "px";
+        if (mapArray[playerRow][playerColumn - 1] !== 'W') {
+            player.style.left = (playerLeft -= 30) + "px";
             playerColumn -= 1;
         }
     }
