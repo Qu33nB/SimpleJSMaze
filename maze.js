efbddef42da3b1e959ebb5a208549ac8bbe55f04
Brandi Cotton <bdcotton52@gmail.com>
2019-10-16 11:50:48 -0500

    Completed win conditions

--- maze.js
+++ maze.js
@@ -53,10 +53,11 @@ function defineCells() {
         cell.classList.add('start');
         cell.innerHTML = '';
     } else if (cell.innerHTML === 'F') {
-        cell.classList.add('finish')
+        cell.id = 'finish';
+        cell.classList.add('finish');
         cell.innerHTML = '';
     } else {
-        cell.classList.add('floor')
+        cell.classList.add('floor');
     }
 }
 
@@ -70,7 +71,7 @@ function addPlayer() {
 addPlayer();
 
 function movePlayer(event) {
-
+console.log(player.style.left)
     if (event.keyCode === 40) {
         if (mapArray[playerRow + 1][playerColumn] !== 'W') {
             player.style.top = (playerTop += 35) + "px";
@@ -88,23 +89,24 @@ function movePlayer(event) {
         }
     } else if (event.keyCode === 37) {
         if (mapArray[playerRow][playerColumn - 1] !== 'W') {
+            if (player.style.left > `0px`) { 
             player.style.left = (playerLeft -= 30) + "px";
             playerColumn -= 1;
         }
+        }
+    }
+    checkForWin()
+}
+
+function checkForWin() {
+    if (player.style.left === `600px`) {
+        document.removeEventListener('keydown', movePlayer); 
+        let overlay = document.createElement('span');
+        overlay.classList.add('overlay');
+        overlay.innerHTML = `Congratulations, you win!`
+        document.body.appendChild(overlay);
     }
 }
 
-
-
-
-
-// Either A) use an absolutely-positioned DIV to represent the player's current position in the maze, or B) have your player DIV appended to a cell DIV for the same reason.
-// You need to keep track of (or retrieve on demand) the player's current position in the maze (row index and column index). You could do this one of several ways. You could keep a persistent record of the player's position in, say, a global array or object whose sole job is keeping track of the player's current position. You could constantly update your map array to reflect your player's movement (move the "S" around). You could keep your indexes in data attributes in your HTML and access them through player DIV's "parentElement" property (in the case of 3B). Or you could do a little math on the player DIV's current position on the screen, relative to the start element's current position on the screen and the size of your cells (in the case of 3A).
-// Movement can be performed a couple different ways: In the case of 3A, change the absolute position of the player DIV. Or, in the case of 3B, append the player DIV to the next cell DIV. (You could use "document.querySelector()", and the CSS selector for attributes to get the next cell element by the indexes you set on it via data attributes.)
-
-// Requirements
-
-// The player must start on the start square.
-// Set up an event handler(s) to move your player DIV around the same way you did for the previous assessment on keyboard events.
 // Don't allow the player to move into a wall or outside the boundary of the maze.
 // When the player moves onto the finish square, show the user that they have won (don't use console.log or alert for this).
